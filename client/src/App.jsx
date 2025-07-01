e9adf59026e5f1548716eb7a9ade50820df724d6
diablom14 <divyanshuasadeveloper@gmail.com>
2025-07-01 16:55:08 +0530

    Refactor App component to include routing and conditional rendering of Navbar and Footer; remove Header component and organize component exports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import {Footer} from "./components"
+import {Favorite, Home, MovieDetails, Movies, MyBookings, SeatLayout} from "./pages"
+import {Toaster} from "react-hot-toast"
+
 
 function App() {
+  const isAdminRoute = useLocation().pathname.startsWith("/admin")
   return (
-    <div>App</div>
+    <>
+      <Toaster/>
+      {!isAdminRoute && <Navbar />}
+      
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/movies' element={<Movies/>}/>
+          <Route path='/movies/:id' element={<MovieDetails/>}/>
+          <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
+          <Route path='/my-bookings' element={<MyBookings/>}/>
+          <Route path='/favorite' element={<Favorite/>}/>
+      </Routes>
+
+      {!isAdminRoute && <Footer/>}
+    </>
   )
 }
 
---
+++ client/src/pages/index.js
@@ -0,0 +1,15 @@
+import Home from "./Home"
+import Favorite from "./Favorite"
+import MovieDetails from "./MovieDetails"
+import Movies from "./Movies"
+import MyBookings from "./MyBookings"
+import SeatLayout from "./SeatLayout"
+
+export {
+    Home,
+    Favorite,
+    Movies,
+    MovieDetails,
+    SeatLayout,
+    MyBookings
+}
